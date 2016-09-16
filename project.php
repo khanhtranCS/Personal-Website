536ea437eb2c7df23a2b5a36d834606487b7076c
Khanh Duc Tran <ktran035@uw.edu>
2016-09-17 02:30:12 +0700

    edited .htaccess file to add trailing slash at the end of url

--- project.php
+++ project.php
@@ -12,7 +12,8 @@ getHeader();
 		</li>
 		<li><a href="https://github.com/khanhtranCS/Chrome-Extension---Translator" target="_blank">English-Vietnamese Translator</a><div id="detail">A Chrome extension that allowed user to define a word by double click while surfing on their favorite webpages.</div></li>
 		<li><a href="https://github.com/khanhtranCS/Chrome-Extension---Translator" target="_blank">Classic Sliding Puzzle</a><div id="detail">A classic sliding puzzle game that clients can use to kill their spare time.</div></li>
-		<li><a href="https://github.com/khanhtranCS/Chrome-Extension---Translator" target="_blank">Facebook to Chrome Notifications</a><div id="detail">A Chrome extension that allow user to access their notifications via this extension even when their facebook tab is closed</div></li>
+		<li><a href="https://github.com/khanhtranCS/Chrome-Extension---Translator" target="_blank">Facebook to Chrome Notifications</a><div id="detail">A Chrome extension that allow user to access their notifications via this extension even when their facebook tab is closed.</div></li>
+		<li><a href="https://github.com/khanhtranCS/Personal-Website" target="_blank">Personal Website</a><div id="detail">My Personal Website that built from scratch.</div></li>
 
 	</ul>
 </div>
